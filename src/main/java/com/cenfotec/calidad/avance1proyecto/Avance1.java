0fb88ce01ed53dbc3770ba3063fb1e10b967dd91
rmeza019 <rmeza013@gmail.com>
2019-10-10 22:48:04 -0600

    agrega comentario pruebas y arregla esBisiesto

--- src/main/java/com/cenfotec/calidad/avance1proyecto/Avance1.java
+++ src/main/java/com/cenfotec/calidad/avance1proyecto/Avance1.java
@@ -4,7 +4,8 @@ public class Avance1 {
     
     public static void main(String[] args) {
         Calendario calendario = new Calendario();
-        System.out.println(calendario.esBisiesto(2020));
+        System.out.println(calendario.esBisiesto(2000));
+        System.out.println(calendario.esBisiesto(2019));
         System.out.println(calendario.esFechaValida(2019,2,29));
         System.out.println(calendario.esFechaValida(2020,2,29));
         System.out.println(calendario.diaSiguiente(2019,2,28));
