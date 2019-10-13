0ffa20f32a0ac7cc639d2e3b2b6c85992587a90a
Fabio Flores <efabiofm@gmail.com>
2019-10-13 13:18:41 -0600

    retornar fecha en una tripleta y que el main lo pueda imprimir

--- src/main/java/com/cenfotec/calidad/avance1proyecto/Avance1.java
+++ src/main/java/com/cenfotec/calidad/avance1proyecto/Avance1.java
@@ -4,13 +4,24 @@ public class Avance1 {
     
     public static void main(String[] args) {
         Calendario calendario = new Calendario();
+        
         System.out.println(calendario.esBisiesto(2000));
         System.out.println(calendario.esBisiesto(2019));
+        
         System.out.println(calendario.esFechaValida(2019,2,29));
         System.out.println(calendario.esFechaValida(2020,2,29));
-        System.out.println(calendario.diaSiguiente(2019,2,28));
-        System.out.println(calendario.diaSiguiente(2020,2,28));
+        
+        int[] resultadoDiaSiguiente1 = calendario.diaSiguiente(2019,2,28);
+        int[] resultadoDiaSiguiente2 = calendario.diaSiguiente(2020,2,28);
+        
+        System.out.println(imprimirTripletaFecha(resultadoDiaSiguiente1));
+        System.out.println(imprimirTripletaFecha(resultadoDiaSiguiente2));
+        
         System.out.println(calendario.diaSemana(2019,11,24));        
     } 
+    
+    public static String imprimirTripletaFecha(int[] fecha) {
+        return "[" + fecha[0] + ", " + fecha[1] + ", " + fecha[2] + "]";
+    }
     
 }
