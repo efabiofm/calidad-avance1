2e66d0ce85e7e8494f3d2c8cf86a6b2dfe4cf2e6
Fabio Flores <efabiofm@gmail.com>
2019-11-16 01:24:31 -0600

    agregar requerimientos fecha_futuro y fecha_pasado

--- src/test/java/com/cenfotec/calidad/avance1proyecto/CalendarioTest.java
+++ src/test/java/com/cenfotec/calidad/avance1proyecto/CalendarioTest.java
@@ -1,9 +1,10 @@
 package com.cenfotec.calidad.avance1proyecto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalendarioTest {
-    
     @org.junit.jupiter.api.Test
     public void testEsBisiesto() {
         Calendario cal = new Calendario();
@@ -64,5 +65,25 @@ public class CalendarioTest {
         assertEquals(1, cal.diaSemana(1900, 1, 1));
         assertEquals(5, cal.diaSemana(2019, 2, 1));
         assertEquals(3, cal.diaSemana(3000, 1, 1));
+    }
+
+    public void testFechaFutura() {
+        Calendario cal = new Calendario();
+        assertThrows(IllegalArgumentException.class, () -> cal.fechaFutura(new int[]{2019,2,28}, 0));
+        assertThrows(IllegalArgumentException.class, () -> cal.fechaFutura(new int[]{2019,13,32}, 1));
+        assertArrayEquals(new int[]{2019, 3, 1}, cal.fechaFutura(new int[]{2019, 2, 28}, 1));
+        assertArrayEquals(new int[]{2020, 2, 29}, cal.fechaFutura(new int[]{2020, 2, 28}, 1));
+        assertArrayEquals(new int[]{2019, 12, 31}, cal.fechaFutura(new int[]{2019, 12, 30}, 1));
+        assertArrayEquals(new int[]{2020, 1, 1}, cal.fechaFutura(new int[]{2019, 12, 31}, 1));
+    }
+    
+    public void testFechaPasada() {
+        Calendario cal = new Calendario();
+        assertThrows(IllegalArgumentException.class, () -> cal.fechaPasada(new int[]{2019,2,28}, 0));
+        assertThrows(IllegalArgumentException.class, () -> cal.fechaPasada(new int[]{2019,13,32}, 1));
+        assertArrayEquals(new int[]{2019, 2, 28}, cal.fechaPasada(new int[]{2019, 3, 1}, 1));
+        assertArrayEquals(new int[]{2020, 2, 29}, cal.fechaPasada(new int[]{2020, 3, 1}, 1));
+        assertArrayEquals(new int[]{2019, 12, 30}, cal.fechaPasada(new int[]{2019, 12, 31}, 1));
+        assertArrayEquals(new int[]{2019, 12, 31}, cal.fechaPasada(new int[]{2020, 1, 1}, 1));
     }
 }
